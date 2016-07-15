c0e33577d2233104b68c54046189d29606a9931d
Eric Dejonckheere <eric@aya.io>
2016-07-15 14:19:14 +0200

    Initial Commit

---
+++ WeatherMan-Mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WeatherMan-Mac
+//
+//  Created by ERIC DEJONCKHEERE on 15/07/2016.
+//  Copyright © 2016 AYA.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
